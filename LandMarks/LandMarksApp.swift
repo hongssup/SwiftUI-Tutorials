52ccd81221c2e532953fe629a1bd6d9e610260d3
hongssup <hongssup0202@gmail.com>
2023-07-23 18:01:32 +0900

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by SeoYeon Hong on 2023/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
