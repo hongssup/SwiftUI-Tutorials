41b80804ffe2e39f6b86ab2d9e4a65ccb592ae1f
hongssup <hongssup0202@gmail.com>
2024-01-04 00:27:39 +0900

    Use SwiftUI views from other frameworks - MapKit

--- LandMarks/CircleImage.swift
+++ LandMarks/CircleImage.swift
@@ -18,8 +18,6 @@ struct CircleImage: View {
     }
 }
 
-struct CircleImage_Previews: PreviewProvider {
-    static var previews: some View {
-        CircleImage()
-    }
+#Preview {
+    CircleImage()
 }
