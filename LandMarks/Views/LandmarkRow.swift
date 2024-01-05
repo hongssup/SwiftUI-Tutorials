cac25bbb7b8e0558c8cab5d6aea81445d6ec918e
hongssup <hongssup0202@gmail.com>
2024-01-06 00:15:41 +0900

    Create the row view

---
+++ LandMarks/Views/LandmarkRow.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkRow.swift
+//  LandMarks
+//
+//  Created by SeoYeon Hong on 1/6/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
