d2f578dfe424caa9247733e19844dbd2a49fb406
hongssup <hongssup0202@gmail.com>
2024-01-07 14:36:02 +0900

    Mark favorite landmarks & filter the list view

--- LandMarks/Views/LandmarkList.swift
+++ LandMarks/Views/LandmarkList.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoriteOnly = true
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoriteOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationSplitView {
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
